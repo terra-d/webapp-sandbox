ae1dd5a4df4a3df0b71af19a2fdf98f64b8c3ae8
terra-d <terra-d@dummy.com>
2022-03-27 01:11:13 +0900

    add sample component

---
+++ vue-2/src/components/SampleForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card class="mx-auto" max-width="350">
+    <v-card-text>
+      <v-form>
+        <v-text-field v-model="name" :counter="10" label="Name" />
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  setup() {
+    const name = ref('');
+
+    return { name };
+  },
+});
+</script>
--- vue-2/src/views/ClickOutsideTest.vue
+++ vue-2/src/views/ClickOutsideTest.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <sample-form />
+    <v-snackbar v-model="snackbar" :timeout="1000"> Clicked outside! </v-snackbar>
+  </v-container>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api';
+import SampleForm from '@/components/SampleForm.vue';
+
+export default defineComponent({
+  name: 'ClickOutsideTest',
+  components: { SampleForm },
+  setup() {
+    const snackbar = ref(false);
+    return { snackbar };
+  },
+});
+</script>
